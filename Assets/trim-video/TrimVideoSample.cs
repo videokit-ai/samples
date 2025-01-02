b90b1cbd1020b64a2f1c342914d85293b7f51945
Yusuf Olokoba <olokobayusuf@gmail.com>
2025-01-02 13:20:53 -0500

    Add video trimming sample

--- Assets/trim-video/TrimVideoSample.cs
+++ Assets/trim-video/TrimVideoSample.cs
@@ -28,6 +28,9 @@ namespace VideoKit.Samples {
         private async void Start () {
             // Load video
             var asset = await MediaAsset.FromStreamingAssets(videoPath);
+            // Calculate start and end times in nanoseconds
+            var startTimeNs = (long)(trimStart * 1e+9f);
+            var endTimeNs = startTimeNs + (long)(trimDuration * 1e+9f);
             // Create a recorder
             var recorder = await MediaRecorder.Create(
                 format: MediaRecorder.Format.MP4,
@@ -35,11 +38,12 @@ namespace VideoKit.Samples {
                 height: asset.height,
                 frameRate: asset.frameRate
             );
-            // Calculate start and end times in nanoseconds
-            var startTimeNs = (long)(trimStart * 1e+9f);
-            var endTimeNs = startTimeNs + (long)(trimDuration * 1e+9f);
             // Enumerate and copy frames
-            using var pixelData = new NativeArray<byte>(asset.width * asset.height * 4, Allocator.Persistent);
+            await Awaitable.BackgroundThreadAsync();
+            using var pixelData = new NativeArray<byte>(
+                length: asset.width * asset.height * 4,
+                allocator: Allocator.Persistent
+            );
             foreach (var pixelBuffer in asset.Read<PixelBuffer>()) {
                 // Check start time
                 if (pixelBuffer.timestamp < startTimeNs)
@@ -65,7 +69,11 @@ namespace VideoKit.Samples {
             // Finish writing
             var trimmedAsset = await recorder.FinishWriting();
             // Playback the trimmed video
+            await Awaitable.MainThreadAsync();
             videoPlayer.url = trimmedAsset.path;
+            videoPlayer.Prepare();
+            while (!videoPlayer.isPrepared)
+                await Awaitable.NextFrameAsync();
             videoPlayer.Play();
             rawImage.texture = videoPlayer.texture;
             aspectFitter.aspectRatio = (float)trimmedAsset.width / trimmedAsset.height;
